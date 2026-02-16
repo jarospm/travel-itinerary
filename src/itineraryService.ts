61d39c1faf1e54b4461734409d2a0fdcdde0c56a
Robbhedonic <143946750+Robbhedonic@users.noreply.github.com>
2026-02-16 10:51:05 +0100

    Add addActivity function to itineraryService

--- src/itineraryService.ts
+++ src/itineraryService.ts
@@ -1,7 +1,11 @@
-import { Trip } from './models';
+import { Activity, Trip } from './models.js';
 
 export const calculateTotalCost = (trip: Trip): number => {
   return trip.activities.reduce((sum, activity) => {
     return sum + activity.cost;
   }, 0);
+};
+
+export const addActivity = (trip: Trip, activity: Activity): void => {
+  trip.activities.push(activity);
 };
