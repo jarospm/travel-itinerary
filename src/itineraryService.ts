5531f8f32c263d8f2a4156a27ee4d0fc869dfe14
Robbhedonic <143946750+Robbhedonic@users.noreply.github.com>
2026-02-17 13:49:36 +0100

    Add getHighCostActivities function to itineraryService

--- src/itineraryService.ts
+++ src/itineraryService.ts
@@ -1,7 +1,14 @@
-import { Trip } from './models.js';
+import { Trip, Activity } from './models.js';
 
 export const calculateTotalCost = (trip: Trip): number => {
   return trip.activities.reduce((sum, activity) => {
     return sum + activity.cost;
   }, 0);
+};
+
+export const getHighCostActivities = (
+  trip: Trip,
+  minimumCost: number,
+): Activity[] => {
+  return trip.activities.filter((activity) => activity.cost >= minimumCost);
 };
